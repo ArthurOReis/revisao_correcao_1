debb49429ec61e8eebea69b9fef255832acd849c
ArthurOReis <121455711+ArthurOReis@users.noreply.github.com>
2024-10-28 09:24:24 -0300

    Tirando limite de leitura do scanf

--- main.c
+++ main.c
@@ -5,17 +5,11 @@ void texto_invalido() {
     printf("Mensagem inválida\n");
 }
 
-int compara_resposta(char *resposta) {
-    return (strcmp(resposta, "sim") == 0) || (strcmp(resposta, "Sim") == 0) ||
-           (strcmp(resposta, "não") == 0) || (strcmp(resposta, "nao") == 0) ||
-           (strcmp(resposta, "Nao") == 0) || (strcmp(resposta, "Não") == 0);
-}
-
 int main() {
     char resposta[10];
 
     puts("Possui filhos?");
-    scanf("%9s", resposta);
+    scanf("%s", resposta);
 
     if (strcmp(resposta, "sim") == 0) {
         puts("Mais de 3 filhos?");
@@ -31,13 +25,13 @@ int main() {
 
     } else if (strcmp(resposta, "não") == 0) {
         puts("Assiste Formula 1?");
-        scanf("%9s", resposta);
+        scanf("%s", resposta);
 
         if (strcmp(resposta, "sim") == 0) {
             puts("Sugestão: Esportiva");
         } else if (strcmp(resposta, "não") == 0) {
             puts("Gosta de campo?");
-            scanf("%9s", resposta);
+            scanf("%s", resposta);
 
             if (strcmp(resposta, "sim") == 0) {
                 puts("Sugestão: Offroad");
